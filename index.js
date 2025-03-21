b3d6463b9d1d7b6369ebd256773bc247da6f2c7b
ArtemKosyrev <artemkosyrev93@gmail.com>
2025-03-21 16:11:33 +0500

    feat: (docker-compose) add postgres

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {PrismaClient} = require("./generated/prisma_client");
+const {PrismaClient} = require('@prisma/client')
 
 const app = express();
 const prisma = new PrismaClient();
